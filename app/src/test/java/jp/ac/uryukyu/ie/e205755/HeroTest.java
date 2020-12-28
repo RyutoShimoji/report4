a2d353cffae9700b2f0b3d9768161b78d6a4ad99
Ryuto Shimoji <e205755@ie.u-ryukyu.ac.jp>
2020-12-28 21:35:09 +0900

    third commit

---
+++ app/src/test/java/jp/ac/uryukyu/ie/e205755/HeroTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e205755;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeroTest {
+    @Test
+    void attackWithWeponSkill() {
+        int defaultWarriorAttack = 100;
+        int wantattackWithWeponSkill= (int)(defaultWarriorAttack * 1.5);
+        Warrior demoWarrior = new Warrior("デモ勇者", 10, defaultWarriorAttack);
+        Enemy slime = new Enemy("スライムもどき", 500, 100);
+
+        for (int i=0; i<3; i++){
+            int beforeAttackEnemyHp = slime.getHitPoint();
+            demoWarrior.attackWithWeponSkill(slime);
+            int afterAttackEnemyHp = slime.getHitPoint();
+            assertEquals(wantattackWithWeponSkill, beforeAttackEnemyHp - afterAttackEnemyHp);
+        }
+    }
+}
